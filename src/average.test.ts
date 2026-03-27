c19e684b0d1337816e6183e863ff9f95fee95363
zainab kalejaiye <zainab_kalejaiye@sluz.ch>
2026-03-27 15:18:48 +0100

    Aufgabe 5.3: testfall für leeres array erstellen, mean() func throws exeption

--- src/average.test.ts
+++ src/average.test.ts
@@ -23,3 +23,11 @@ test("test mean of five numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act & Assert
+  expect(() => mean(numbers)).toThrow("Cannot calculate mean of empty array");
+});
--- src/average.ts
+++ src/average.ts
@@ -1,8 +1,13 @@
 export function mean(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Cannot calculate mean of empty array");
+  }
+
   let sum: number = 0;
   for (let number of numbers) {
     sum += number;
   }
+
   const mean: number = sum / numbers.length;
   return mean;
 }
